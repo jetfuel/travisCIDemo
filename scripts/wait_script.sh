85a2e939225d434d23887438cacbfcd0936bd28e
Wang,Jeff <wangjeff@baidu.com>
2018-10-02 16:30:58 -0700

    1

--- scripts/wait_script.sh
+++ scripts/wait_script.sh
@@ -3,8 +3,7 @@ set -e
 
 echo "Hello, I am a wait script that exit 0"
 
-ls
-echo $HOME
+ls $HOME
 
 
 if [ ! -d $HOME/temp ] ; then
@@ -18,33 +17,33 @@ echo "------"
 
 sleep 50
 echo 1
-echo $RANDOM > temp/test.txt
+echo $RANDOM > $HOME/temp/test.txt
 
 cat temp/test.txt
 
 sleep 50
 echo 2
-echo "Hello 2" >> temp/test.txt
-echo $RANDOM > temp/test.txt
+echo "Hello 2" >> $HOME/temp/test.txt
+echo $RANDOM > $HOME/temp/test.txt
 
 cat temp/test.txt
 sleep 50
 echo 3
-echo "Hello 3" >> temp/test.txt
-echo $RANDOM > temp/test.txt
+echo "Hello 3" >> $HOME/temp/test.txt
+echo $RANDOM > $HOME/temp/test.txt
 
 cat temp/test.txt
 sleep 50
 echo 4
-echo "Hello 4" >> temp/test.txt
-echo $RANDOM > temp/test.txt
+echo "Hello 4" >> $HOME/temp/test.txt
+echo $RANDOM > $HOME/temp/test.txt
 
 sleep 50
 echo 5
-echo "Hello 5" >> temp/test.txt
+echo "Hello 5" >> $HOME/temp/test.txt
 
 sleep 50
 echo 6
-echo "Hello 6" >> temp/test.txt
+echo "Hello 6" >> $HOME/temp/test.txt
 
 exit 0
