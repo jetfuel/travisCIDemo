3133969a706dd1864a09e55159bd42434d75c2f2
Wang,Jeff <wangjeff@baidu.com>
2018-10-02 14:28:49 -0700

    testing cache

---
+++ scripts/wait_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+echo "Hello, I am a wait script that exit 0"
+
+
+if [ ! -d temp ] ; then
+mkdir temp
+touch temp/test.txt
+fi
+
+cat temp/test.txt
+
+sleep 50
+echo 1
+echo "Hello 1" > temp/test.txt
+
+sleep 50
+echo 2
+echo "Hello 2" > temp/test.txt
+
+sleep 50
+echo 3
+echo "Hello 3" > temp/test.txt
+
+sleep 50
+echo 4
+echo "Hello 4" > temp/test.txt
+
+sleep 50
+echo 5
+echo "Hello 5" > temp/test.txt
+
+sleep 50
+echo 6
+echo "Hello 6" > temp/test.txt
+
+exit 0
